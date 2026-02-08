20dd2a3b6931eaa6ff49d8d990a4646730d18a35
31b4 <szilagyibence@szilagyibence.com>
2026-02-08 11:43:11 +0100

    Initial Commit

---
+++ Protocol/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Protocol
+//
+//  Created by Bence Szilagyi on 2/8/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
